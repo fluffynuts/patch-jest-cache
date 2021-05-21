8b869be91f04b23690119175577c0d15fe53acd0
Davyd McColl <davydm@gmail.com>
2021-05-21 16:56:27 +0200

    :memo: add todos so I know where to pick up again (:

--- tests/tweak-jest-cache.spec.ts
+++ tests/tweak-jest-cache.spec.ts
@@ -13,4 +13,63 @@ describe(`tweak-jest-cache`, () => {
             // Assert
         });
     });
+
+    // TODO: readCacheFile and writeCacheFile should be opportunistic. A cache
+    // is is NO reason to completely bail out. Cache is to improve performance,
+    // not a lynchpin for the entire process.
+
+    // TODO: replace writeCacheFile with opportunistic cacher:
+    // original code is:
+    // const writeCacheFile = (cachePath, fileData) => {
+    //     try {
+    //         (0, _writeFileAtomic().sync)(cachePath, fileData, {
+    //             encoding: 'utf8',
+    //             fsync: false
+    //         });
+    //     } catch (e) {
+    //         if (cacheWriteErrorSafeToIgnore(e, cachePath)) {
+    //             return;
+    //         }
+    //
+    //         e.message =
+    //             'jest: failed to cache transform results in: ' +
+    //             cachePath +
+    //             '\nFailure message: ' +
+    //             e.message;
+    //         removeFile(cachePath);
+    //         throw e;
+    //     }
+    // };
+
+    // TODO: replace readCacheFile with opportunistic cache reader
+    // original code is:
+    // const readCacheFile = cachePath => {
+    //     if (!fs().existsSync(cachePath)) {
+    //         return null;
+    //     }
+    //
+    //     let fileData;
+    //
+    //     try {
+    //         fileData = fs().readFileSync(cachePath, 'utf8');
+    //     } catch (e) {
+    //         e.message =
+    //             'jest: failed to read cache file: ' +
+    //             cachePath +
+    //             '\nFailure message: ' +
+    //             e.message;
+    //         removeFile(cachePath);
+    //         throw e;
+    //     }
+    //
+    //     if (fileData == null) {
+    //         // We must have somehow created the file but failed to write to it,
+    //         // let's delete it and retry.
+    //         removeFile(cachePath);
+    //     }
+    //
+    //     return fileData;
+    // };
+
+
 });
