c19c7d460012d88c473c8066b086d4fba56f6f04
Davyd McColl <davydm@gmail.com>
2021-05-25 14:46:09 +0200

    :bug: --revert should attempt to revert

--- src/patch-jest-cache.ts
+++ src/patch-jest-cache.ts
@@ -1,5 +1,5 @@
 import { PatchOptions } from "./gather-args";
-import { backUpScriptTransformer, restoreScriptTransformer } from "./back-up-script-transformer";
+import { backUpScriptTransformer, RestoreResults, restoreScriptTransformer } from "./back-up-script-transformer";
 import { findScriptTransformer } from "./find-script-transformer";
 import { readScriptTransformer } from "./read-script-transformer";
 import { writeTextFile } from "yafs";
@@ -9,11 +9,13 @@ import { resolve } from "path";
 
 export async function patchJestCache(options: PatchOptions): Promise<void> {
     process.chdir(resolve(options.in));
-
     const
         scriptTransformerPath = await findScriptTransformer(),
         ctx = new ExecStepContext();
-    await ctx.exec(`patching ${scriptTransformerPath}`, async () => {
+    if (options.revert) {
+        return revert(ctx, scriptTransformerPath);
+    }
+    await ctx.exec(`patching ${ scriptTransformerPath }`, async () => {
         await restoreScriptTransformer(); // ignore errors & always start from scratch
         await backUpScriptTransformer(); // always have a backup before modifying
         const
@@ -23,3 +25,21 @@ export async function patchJestCache(options: PatchOptions): Promise<void> {
     });
 }
 
+async function revert(
+    ctx: ExecStepContext,
+    scriptTransformerPath: string
+): Promise<void> {
+    const finalResult = await ctx.exec(`reverting ${ scriptTransformerPath }`,
+        async () => {
+            const result = await restoreScriptTransformer();
+            if (result === RestoreResults.fail) {
+                throw new Error(`Unable to restore ${ scriptTransformerPath }`);
+            }
+            return result;
+        }
+    );
+    if (finalResult === RestoreResults.noBackupFound) {
+        console.warn(`No backup was found for ${ scriptTransformerPath } - have you patched it before?`);
+    }
+    return;
+}
